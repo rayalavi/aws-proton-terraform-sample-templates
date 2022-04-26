a2e1e7bd0e6bfe7a4907aab237d61a0e2229a2f0
Ryan Racicot <racicot@amazon.com>
2022-04-26 13:55:55 -0400

    Add scheduled-lambda-svc pipeline from mike

--- service-templates/scheduled-lambda-svc/dev-resources/proton.variables.tf
+++ service-templates/scheduled-lambda-svc/dev-resources/proton.variables.tf
@@ -1,21 +1,46 @@
-variable "environment" {
+
+variable "service_instances" {
+  type = list(
+  object({
+    name    = string
+    inputs  = map(string)
+    outputs = map(string)
+    environment = object({
+      account_id = string
+      name       = string
+      outputs    = map(string)
+    })
+  })
+  )
+}
+
+variable "service_instance" {
   type = object({
-    account_id = string
-    name       = string
-    outputs    = map(string)
+    name   = string
+    inputs = map(string)
   })
   default = null
 }
 
 variable "service" {
   type = object({
-    name = string
+    name                      = string
+    repository_id             = string
+    repository_connection_arn = string
+    branch_name               = string
   })
+  default = null
 }
 
-variable "service_instance" {
+variable "environment" {
   type = object({
-    name   = string
+    outputs = map(string)
+  })
+  default = null
+}
+
+variable "pipeline" {
+  type = object({
     inputs = map(string)
   })
   default = null
